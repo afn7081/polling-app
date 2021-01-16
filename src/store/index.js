22a3635278cf875d0f31662ce5a0d86d27fc4099
unknown <khan.afnan1117@gmail.com>
2021-01-16 20:03:36 +0530

    completed v-1

--- src/store/index.js
+++ src/store/index.js
@@ -16,7 +16,6 @@ export const store=createStore(
     rootReducer,
     DEFAULT_STATE,
     compose(
-        applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__()
+        applyMiddleware(thunk)
         )
 );
